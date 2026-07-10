b56a7af55e03b9214f03274bbff23419e338ebcd
sapna-adsul <sapnaadsul15@gmail.com>
2026-07-10 23:55:22 +0530

    updated missing num

--- missingnum.js
+++ missingnum.js
@@ -1,9 +1,12 @@
+//given : arr=[1,2,3,5,6] len=6
+
 let arr = [1, 2, 3, 5];
-let missing = 0;
-for (let i of arr) {
-  if (arr[i + 1] - arr[i] != -1) {
-    missing = arr[i] - 1;
-  }
+let len = 5
+let total = (len * (len + 1)) / 2;
+let sum = 0
+for (let n of arr) {
+  sum += n;
 }
 
-console.log("missing number = ", missing);
+console.log("missing number = " + (total - sum));
+
