f57836ab1eec81c516d9ea1e12b15a5b81339154
sapna-adsul <sapnaadsul15@gmail.com>
2026-07-10 22:45:47 +0530

    updated merge zeroes

--- movezeroes.js
+++ movezeroes.js
@@ -1,21 +1,22 @@
 let arr = [1, 0, 3, 4, 6, 0, 8, 7];
 let zeroCount = 0;
 
-// let arr1 = [];
-// for (let i in arr) {
-//   if (arr[i] != 0) {
-//     arr1.push(arr[i]);
-//   } else {
-//     zeroCount++;
-//   }
-// }
+//approach 1
+let arr1 = [];
+for (let n of arr) {
+  if (n !== 0) {
+    arr1.push(n);
+  } else {
+    zeroCount++;
+  }
+}
+while (zeroCount > 0) {
+  arr1.push(0);
+  zeroCount--;
+}
+console.log(arr1);
 
-// while (zeroCount > 0) {
-//   arr1.push(0);
-//   zeroCount--;
-// }
-
-// console.log(arr1);
+//Approach 2
 let j = 0; //move nozero elements at this position
 for (let i = 0; i < arr.length; i++) {
   if (arr[i] !== 0) {
